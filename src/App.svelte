9be4549cd849ddbe825ee214e1747054c9aeeb6f
shawncocklin <shawncocklin@gmail.com>
2022-04-14 19:07:31 -0400

    iniital commit, some scaffolding

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import DesignSystem from './components/DesignSystem.svelte'
+</script>
+
+<main class="container bg-dark">
+  <DesignSystem />
+</main>
+
+<style>
+</style>
---
+++ src/components/DesignSystem.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Colors from './Colors.svelte'
+</script>
+
+<h1>Design System</h1>
+
+<section id="colors">
+  <Colors />
+</section>
