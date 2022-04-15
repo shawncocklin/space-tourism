2b42aaf4c771686c09959de711b46107df15f319
shawncocklin <shawncocklin@gmail.com>
2022-04-15 17:57:02 -0400

    stuff broke

---
+++ src/components/Colors.svelte
@@ -0,0 +1,6 @@
+<h2><span>01</span>Colors</h2>
+<div class="flex">
+  <div style="padding: 3rem 1rem 1rem; border: 1px solid white;" />
+  <div style="padding: 3rem 1rem 1rem; border: 1px solid white;" />
+  <div style="padding: 3rem 1rem 1rem; border: 1px solid white;" />
+</div>
