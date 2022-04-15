cc6c86ab9ef8366888693758605a90175ae5cc86
shawncocklin <shawncocklin@gmail.com>
2022-04-15 18:36:51 -0400

    component scaffolded out

--- src/components/Colors.svelte
+++ src/components/Colors.svelte
@@ -1,6 +1,38 @@
 <h2><span>01</span>Colors</h2>
-<div class="flex">
-  <div style="padding: 3rem 1rem 1rem; border: 1px solid white;" />
-  <div style="padding: 3rem 1rem 1rem; border: 1px solid white;" />
-  <div style="padding: 3rem 1rem 1rem; border: 1px solid white;" />
+
+<div class="grid main-grid">
+  <div class="flex flex-column">
+    <div class="bg-dark text-white" style="padding: 3rem 1rem 1rem; border: 1px solid white;">
+      <p>#0B0D17</p>
+    </div>
+    <p><span class="text-light">RGB</span> 11, 13, 23</p>
+    <p><span class="text-light">HSL</span> 230&deg;, 35%, 7%</p>
+  </div>
+
+  <div class="flex flex-column">
+    <div class="bg-light text-dark" style="padding: 3rem 1rem 1rem; border: 1px solid white;">
+      <p>#D0D6F9</p>
+    </div>
+    <p><span class="text-light">RGB</span> 208, 214, 249</p>
+    <p><span class="text-light">HSL</span> 231&deg;, 77%, 90%</p>
+  </div>
+
+  <div class="flex flex-column">
+    <div class="bg-white text-dark" style="padding: 3rem 1rem 1rem; border: 1px solid white;">
+      <p>#FFFFFF</p>
+    </div>
+    <p><span class="text-light">RGB</span> 255, 255, 255</p>
+    <p><span class="text-light">HSL</span> 0&deg;, 0%, 100%</p>
+  </div>
 </div>
+
+<style>
+  div.main-grid {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  span {
+    display: inline-block;
+    margin-right: 3em;
+  }
+</style>
