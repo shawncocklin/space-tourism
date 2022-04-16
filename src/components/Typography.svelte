62c535f1482dbfba1eb1b29f5922fad34dedc982
shawncocklin <shawncocklin@gmail.com>
2022-04-16 12:47:11 -0400

    simplified typography css classes

--- src/components/Typography.svelte
+++ src/components/Typography.svelte
@@ -3,23 +3,23 @@
 </h2>
 
 <p class="text-light fs-400">Heading 1 - Bellefair Regular - 150px</p>
-<p class="h1 uppercase">Earth</p>
+<p class="fs-xxl font-serif uppercase">Earth</p>
 <p class="text-light fs-400">Heading 2 - Bellefair Regular - 100px</p>
-<p class="h2 uppercase">Venus</p>
+<p class="fs-xl font-serif uppercase">Venus</p>
 <p class="text-light fs-400">Heading 3 - Bellefair Regular - 56px</p>
-<p class="h3 uppercase">Jupiter & Saturn</p>
+<p class="fs-lg font-serif uppercase">Jupiter & Saturn</p>
 <p class="text-light fs-400">Heading 4 - Bellefair Regular - 32px</p>
-<p class="h4 uppercase">Uranus, Neptune, & Pluto</p>
+<p class="fs-900 font-serif uppercase">Uranus, Neptune, & Pluto</p>
 <p class="text-light fs-400">Heading 5 - Barlow Condensed Regular - 28px - 4.75 Character Space</p>
-<p class="h5 uppercase">So, you want to travel to space</p>
+<p class="text-light fs-800 letter-gap-lg font-sans uppercase">So, you want to travel to space</p>
 <p class="text-light fs-400">Subheading 1 - Bellefair Regular - 28px</p>
-<p class="subheading-one uppercase">384,400 KM</p>
+<p class="fs-800 font-serif uppercase">384,400 KM</p>
 <p class="text-light fs-400">
   Subheading 2 - Barlow Condensed Regular - 14px - 2.35 Character Spcace
 </p>
-<p class="subheading-two uppercase">avg. distance</p>
+<p class="fs-xs letter-gap-sm font-sans uppercase">avg. distance</p>
 <p class="text-light fs-400">Nav Text - Barlow Condensed Regular - 16px - 2.7 Character Space</p>
-<p class="nav-text uppercase">Europa</p>
+<p class="fs-300 letter-gap-md font-sans uppercase">Europa</p>
 <p class="text-white-muted fs-400">Body Text</p>
 <p>
   Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque
