39636b9ff90ea8add6ed56084e63d1a3ae0374da
shawncocklin <shawncocklin@gmail.com>
2022-04-21 08:29:32 -0400

    finished nav bar minus state changes

--- src/components/Interactive.svelte
+++ src/components/Interactive.svelte
@@ -2,24 +2,38 @@
   <span class="heading-span text-white-muted">03</span>Interactive Elements
 </h2>
 
-<nav>
-  <ul class="flex bg-white-muted">
-    <li><a class="text-white font-sans fs-600 letter-gap-md" href="/"><span>01 </span>Active</a></li>
-    <li><a class="text-white font-sans fs-600 letter-gap-md" href="/"><span>02 </span>Hovered</a></li>
-    <li><a class="text-white font-sans fs-600 letter-gap-md" href="/"><span>03 </span>Idle</a></li>
+<div class="flow" style="--spacer: 2rem;">
+  <nav class="bg-white-muted" style="padding: 2rem 0;">
+    <ul class="main-nav flex justify-between" role="list">
+      <li><a class="nav-link uppercase" href="/"><span class="bold">00 </span>Active</a></li>
+      <li><a class="nav-link uppercase" href="/"><span class="bold">01 </span>Hovered</a></li>
+      <li><a class="nav-link uppercase" href="/"><span class="bold">02 </span>Idle</a></li>
+    </ul>
+  </nav>
+
+  <p class="text-light fs-400 text-center">Different States of Navigation Bar</p>
+</div>
+<div>
+  <div>
+    <button>Explore</button>
+    <button>Explore</button>
+  </div>
+  <ul>
+    <li><a href="/">Moon</a></li>
+    <li><a href="/">Mars</a></li>
+    <li><a href="/">Europa</a></li>
   </ul>
-</nav>
+  <ul>
+    <li><a href="/">0</a></li>
+    <li><a href="/">0</a></li>
+    <li><a href="/">0</a></li>
+  </ul>
+  <ul>
+    <li><a href="/">1</a></li>
+    <li><a href="/">2</a></li>
+    <li><a href="/">3</a></li>
+  </ul>
+</div>
 
 <style>
-  ul {
-    list-style: none;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  span {
-    font-weight: 700;
-  }
 </style>
